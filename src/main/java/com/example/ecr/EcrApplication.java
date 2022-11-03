4d686f1c5d8eeeb08f6bf914a07e2a11d8c514fd
심용현(팀) <yonghyeon.sim@pluglink.kr>
2022-11-03 15:43:26 +0900

    init

---
+++ src/main/java/com/example/ecr/EcrApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcrApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/ecr/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.example.ecr.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public Map<String, String> test() {
+        Map<String, String> map = new HashMap<>();
+        map.put("key", "value");
+        return map;
+    }
+}
